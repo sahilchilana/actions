15dc918104c5e5fc7214ce5471b50773496304af
sahilchilana <44232671+sahilchilana@users.noreply.github.com>
2019-06-27 16:26:08 +0530

    Update entrypoint.sh

--- aws/sam/entrypoint.sh
+++ aws/sam/entrypoint.sh
@@ -1,20 +1,5 @@
 #!/bin/sh
 
-set -e
-
-# Respect AWS_DEFAULT_REGION if specified
-[ -n "$AWS_DEFAULT_REGION" ] || export AWS_DEFAULT_REGION=us-east-1
-
-# Respect AWS_DEFAULT_OUTPUT if specified
-[ -n "$AWS_DEFAULT_OUTPUT" ] || export AWS_DEFAULT_OUTPUT=json
-
-# Capture output
-start=$(date '+%s')
-output=$( sh -c "sam $*" )
-
-# Preserve output for consumption by downstream actions
-echo "$output" > "${HOME}/${GITHUB_ACTION}.${AWS_DEFAULT_OUTPUT}"
-
-# Write output to STDOUT
-echo "$output"
-
+cd lambda_function
+sam build --use-container
+sam local invoke -e functions/add_function/event.json
